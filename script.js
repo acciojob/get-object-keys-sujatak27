bcb57dea3b204c59acf2034fcda93aaa2a66b333
sujatak27 <160421877+sujatak27@users.noreply.github.com>
2024-04-19 07:18:40 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+// Define the student object with a name property
+const student = {
+  name: "Sujata"
+};
+
+// Add a method to the Object prototype called getKeys()
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Test the getKeys() method
+console.log(student.getKeys()); 
